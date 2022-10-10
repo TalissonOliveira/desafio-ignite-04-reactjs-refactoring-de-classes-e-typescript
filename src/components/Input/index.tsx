d09e6f4bb98a4a88b5dd98222ae53bbb0d0c79b5
Talisson Oliveira <talissonlux@outlook.com>
2022-10-10 06:22:59 -0300

    fix: fixes the types of the input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -13,7 +13,7 @@ import { Container } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  icon: ElementType;
+  icon?: ElementType;
 }
 
 function Input({ name, icon: Icon, ...rest }: InputProps) {
